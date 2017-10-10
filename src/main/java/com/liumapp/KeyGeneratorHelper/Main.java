23952848be6e0270512124913fe9d5d3c4320aa0
liumapp <liumapp.com@gmail.com>
2017-10-10 14:49:07 +0800

    add pattern

---
+++ src/main/java/com/liumapp/KeyGeneratorHelper/Main.java
@@ -0,0 +1,20 @@
+package com.liumapp.KeyGeneratorHelper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by liumapp on 9/28/17.
+ * E-mail:liumapp.com@gmail.com
+ * home-page:http://www.liumapp.com
+ */
+@Configuration
+@EnableAutoConfiguration
+public class Main {
+
+
+
+}
